6c7f6bd56c0c0297da82548e8ec9d4992ef62b1e
Roy Lin 林孟憲 <bizroylin@gmail.com>
2024-05-12 00:45:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Eden, Peggy，This is my first Google Cloud project')
+    res.end('Hello Eden and Peggy, This is my first Google Cloud project')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
